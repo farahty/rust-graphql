ee130867ef25c4956066969bf9b5b96bfd131659
Nimer Farahty <nimer@farahty.com>
2022-01-21 18:25:56 +0200

    create brands route

--- src/db.rs
+++ src/db.rs
@@ -1,10 +1,11 @@
 use mongodb::{Client, Database};
 
-pub async fn connect() -> Result<Database, mongodb::error::Error> {
+pub async fn connect() -> Database {
     let db_uri = std::env::var("DB_URI").expect("database uri is not available");
     let db_name = std::env::var("DB_NAME").expect("database name is not available");
 
-    let clint = Client::with_uri_str(&db_uri).await?.database(&db_name);
-
-    Ok(clint)
+    Client::with_uri_str(&db_uri)
+        .await
+        .expect("failed to connect to database")
+        .database(&db_name)
 }
--- src/main.rs
+++ src/main.rs
@@ -7,28 +7,52 @@ use async_graphql::http::{playground_source, GraphQLPlaygroundConfig};
 use async_graphql_poem::GraphQL;
 use dotenv::dotenv;
 
+use futures::TryStreamExt;
+use models::Brand;
+use mongodb::Database;
 use poem::listener::TcpListener;
-use poem::web::Html;
-use poem::{get, handler, IntoResponse, Route, Server};
+use poem::web::{Data, Html, Json};
+use poem::{get, handler, middleware::AddData, EndpointExt, IntoResponse, Route, Server};
 
 #[handler]
 async fn graphql_playground() -> impl IntoResponse {
     Html(playground_source(GraphQLPlaygroundConfig::new("/")))
 }
 
+#[handler]
+async fn get_brands(db: Data<&Database>) -> Json<serde_json::Value> {
+    let brands = db
+        .collection("brands")
+        .find(None, None)
+        .await
+        .unwrap()
+        .try_collect::<Vec<Brand>>()
+        .await
+        .unwrap();
+
+    Json(serde_json::json!(brands))
+}
+
 #[tokio::main]
 async fn main() {
     dotenv().ok();
 
-    let client = db::connect()
-        .await
-        .expect("failed to connect to database ...");
+    let port = std::env::var("PORT").unwrap_or("3000".to_string());
+    let database = db::connect().await;
+    let schema = resolvers::build_schema().data(database.clone()).finish();
+    let app = Route::new()
+        .at("/", get(graphql_playground).post(GraphQL::new(schema)))
+        .at("/brands", get(get_brands))
+        .with(AddData::new(database));
 
-    let schema = resolvers::build_schema().data(client.clone()).finish();
-    let app = Route::new().at("/", get(graphql_playground).post(GraphQL::new(schema)));
+    let server = Server::new(TcpListener::bind(format!("0.0.0.0:{}", port)));
 
-    Server::new(TcpListener::bind("0.0.0.0:8000"))
-        .run(app)
-        .await
-        .unwrap()
+    println!("🚀 Server start at http://localhost:{}/", port);
+
+    if let Err(error) = server.run(app).await {
+        println!(
+            "❌ Failed to start server at http://localhost:{}/ \n {:?}",
+            port, error
+        )
+    }
 }
